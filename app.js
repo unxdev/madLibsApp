7851ba0736493e75f6335d59e14c89322ea06a15
unexpectedsense <javierbaldo@gmail.com>
2022-07-07 01:09:10 -0300

    Final Polish

--- app.js
+++ app.js
@@ -1,24 +1,4 @@
 // Declarations
-
-// the original story for reference
-const originalStory = `
-<p>Alexander Graham Bell was the man who invented the telephone.
-<br>He was born in Edinburgh on 3 March 1847.</p> 
-
-<p>He was educated at Edinburgh High School, Edinburgh University, and the University of London.</p> 
-
-<p>Later Alexander Graham Bell moved to the USA and in 1872 he opened a school for training teachers of the deaf in Boston.</p> 
-
-<p>Bell experimented with ways of using electricity to transmit sound, helped by his assistant Thomas Watson and in 1876 he patented the telephone. 
-<br>He also invented a way to transmit sound using vibrations in a beam of light.</p>
-
-<p>Alexander Graham Bell also carried out experiments in many other subjects and in 1898 he became President of the National Geographical Society. 
-<br>He was also interested in the problem of mechanical flight and experimented with kites.</p>
-
-<p>Bell married a deaf woman in 1877 and they had four children. Bell became a US citizen in 1882 but in 1855 he purchased a summer home in Nova Scotia. 
-<br>There is a crater on the Moon named after Bell.</p>`;
-
-
 // questions to the player
 let questionArray = [
     'anything you can express',
@@ -48,30 +28,44 @@ for (let i = 0; i < numberOfQuestions; i++) {
     questionCounter++;
     console.log(questionArray[i] + `(${numberOfQuestions - questionCounter} questions left)`);
     let insertValue = prompt('Enter a ' + questionArray[i]  + ` (${numberOfQuestions - questionCounter} questions left)`);
+    let iterate = true;
+
+    while (iterate) {
+        if(insertValue.length === 0 || insertValue === undefined){
+            console.log('Please entered a value');
+            insertValue = prompt('Please!! Enter a ' + questionArray[i]  + ` (${numberOfQuestions - questionCounter} questions left)`);
+        }else{
+            iterate = false;
+            break;
+        }
+    }
     userInputs.push(insertValue);
     console.log(userInputs.length);
 }
 
 
-let newStory = `<p>${userInputs[1]} was the man who invented the ${userInputs[4]}.
-<br>He was born in ${userInputs[5]} on 3 March 1847. </p>
+let newStory = `<h1>A Beautiful Story</h1>
+<h2>${userInputs[1]} was the man who invented the ${userInputs[4]}.
+<br>He was born in ${userInputs[5]} on 3 March 1847. </h2>
 
-<p>He was educated at ${userInputs[5]} High School, ${userInputs[5]} University, and the University of ${userInputs[6]}. </p>
+<h2>He was educated at ${userInputs[5]} High School, ${userInputs[5]} University, and the University of ${userInputs[6]}. </h2>
 
-<p>Later ${userInputs[1]} moved to the ${userInputs[7]} and in 1872 he opened a school for training teachers of the deaf in Boston. </p>
+<h2>Later ${userInputs[1]} moved to the ${userInputs[7]} and in 1872 he opened a school for training teachers of the deaf in Boston. </h2>
 
-<p>${userInputs[2]} experimented with ways of using electricity to transmit ${userInputs[0]}, ${userInputs[12]} by his assistant ${userInputs[3]} and in 1876 he patented the ${userInputs[4]}.
-<br>He also invented a way to transmit ${userInputs[0]} using vibrations in a beam of light.</p>
+<h2>${userInputs[2]} experimented with ways of using electricity to transmit ${userInputs[0]}, ${userInputs[12]} by his assistant ${userInputs[3]} and in 1876 he patented the ${userInputs[4]}.
+<br>He also invented a way to transmit ${userInputs[0]} using vibrations in a beam of light.</h2>
 
-<p>${userInputs[1]} also carried out experiments in many other subjects and in 1898 he became ${userInputs[8]} of the National Geographical Society.
-<br>He was also interested in the problem of ${userInputs[9]} and experimented with ${userInputs[10]}.</p>
+<h2>${userInputs[1]} also carried out experiments in many other subjects and in 1898 he became ${userInputs[8]} of the National Geographical Society.
+<br>He was also interested in the problem of ${userInputs[9]} and experimented with ${userInputs[10]}.</h2>
 
-<p>${userInputs[2]} married a deaf woman in 1877 and they had four ${userInputs[11]}. ${userInputs[2]} became a US citizen in 1882 but in 1855 he ${userInputs[13]} a summer home in Nova Scotia.   
-<br>There is a crater on the Moon named after ${userInputs[2]}.</p>`;
+<h2>${userInputs[2]} married a deaf woman in 1877 and they had four ${userInputs[11]}. ${userInputs[2]} became a US citizen in 1882 but in 1855 he ${userInputs[13]} a summer home in Nova Scotia.   
+<br>There is a crater on the Moon named after ${userInputs[2]}.</h2>`;
 
+
+alert('All done! Ready for your totally-accurate, not-at-all confusing history of tech??');
 
 // for debugging purposes
-originalWords = [
+/* originalWords = [
     'sound',
     'Alexander Graham Bell',
     'Bell',
@@ -86,15 +80,9 @@ originalWords = [
     'children',
     'helped',
     'purchased'
-];
-
-userInputs = originalWords;
+]; */
 
 console.log('### NEW Story ');
 console.log(newStory);
-console.log('### originalStory ');
-console.log(originalStory);
 let divOutput = document.querySelector('div.newStory');
 divOutput.innerHTML = newStory;
-let original = document.querySelector('div.original');
-original.innerHTML = originalStory; 
